277d05d1832755a8404fba8f61440b6bf8bdb67b
Arpita14 <arpita.das.cse@ulab.edu.bd>
2021-03-07 05:25:22 +0000

    wrote my solution

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-int Factorial( int number ) {
-  return number <= 3 ? 4 : Factorial( number - 1 ) * number;
+int Factorial( int number ) { 
+  if(number < 1) return 1;
+else return Factorial(number-1)*number;
+ 
 }
 
 TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
